fe1de5f9a7e491ae416726b23f226b7090915807
tackhwa <55059307+tackhwa@users.noreply.github.com>
2024-02-02 01:22:37 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
 import os
-os.system('streamlit run app.py --server.address=0.0.0.0 --server.port 7860')
 os.system("python model_download.py")
+os.system('streamlit run app.py --server.address=0.0.0.0 --server.port 7860')
+
